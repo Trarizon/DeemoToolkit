b8feb32f05c1928d92f46e5d37b87d81b65952fd
Trarizon <44918595+Trarizon@users.noreply.github.com>
2024-01-01 00:31:57 +0800

    use .net 8.0

--- Trarizon.Toolkit.Deemo.Algorithm/ScoreCalculation.cs
+++ Trarizon.Toolkit.Deemo.Algorithm/ScoreCalculation.cs
@@ -3,8 +3,8 @@
 namespace Trarizon.Toolkit.Deemo.Algorithm;
 public static class ScoreCalculation
 {
-	private const float CharmingLimit = 50f;
-	private const float HitLimit = 120;
+	private const float CharmingBoundary = 50f;
+	private const float HitBoundary = 120;
 
 	// Result is in [0, 1]
 	public static float TotalScore(int notesCount, ReadOnlySpan<float> offsets) => CombineScores(JudgeScore(notesCount, offsets), ComboScore(notesCount, offsets));
@@ -49,7 +49,7 @@ public static class ScoreCalculation
 			if (offsets[i].IsCharming())
 				score += 1f;
 			else if (offsets[i].IsHit())
-				score += (HitLimit - offsets[i]) / (HitLimit - CharmingLimit) * 0.5f + 0.5f;
+				score += (HitBoundary - offsets[i]) / (HitBoundary - CharmingBoundary) * 0.5f + 0.5f;
 		}
 		return score;
 	}
@@ -78,6 +78,56 @@ public static class ScoreCalculation
 	private static float ActualFullComboScore(int noteCount) => ((noteCount - 1) * noteCount) >> 1;
 	private static float CombineScores(float judgeScore, float comboScore) => judgeScore * 0.8f + comboScore * 0.2f;
 
-	private static bool IsCharming(this float offset) => offset <= CharmingLimit;
-	private static bool IsHit(this float offset) => offset <= HitLimit;
+	private static bool IsCharming(this float offset) => offset <= CharmingBoundary;
+	private static bool IsHit(this float offset) => offset <= HitBoundary;
+
+	public static ScoreResult Calc(ReadOnlySpan<float> offsets)
+	{
+		ScoreResult result = new() { NoteCount = offsets.Length };
+		float sCombo = -1f; // first hit score is 0;
+
+		int nCombo = 0;
+
+		for (int i = 0; i < offsets.Length; i++) {
+			switch (offsets[i]) {
+				case <= CharmingBoundary:
+					result.JudgeScore += 1f;
+
+					result.ComboScore += sCombo += 1f;
+					nCombo++;
+					result.CharmingCount++;
+					break;
+				case <= HitBoundary:
+					result.JudgeScore += (HitBoundary - offsets[i]) / (HitBoundary - CharmingBoundary) * 0.5f + 0.5f;
+					result.ComboScore += sCombo += 1f;
+
+					nCombo++;
+					break;
+				default:
+					// Didn't hit leading notes
+					if (result.MaxCombo == 0 && nCombo == 0)
+						break;
+
+					if (result.MaxCombo < nCombo) 
+						result.MaxCombo = nCombo;
+					nCombo = 0;
+					sCombo *= 0.7f;
+					break;
+			}
+		}
+		result.JudgeScore /= result.NoteCount;
+		result.ComboScore /= ((result.NoteCount - 1) * result.NoteCount) >> 1;
+		return result;
+	}
+}
+
+public record struct ScoreResult
+{
+	public int CharmingCount;
+	public int MaxCombo;
+	public int NoteCount;
+	public float JudgeScore;
+	public float ComboScore;
+
+	public readonly float Score => JudgeScore * 0.8f + ComboScore * 0.2f;
 }
--- Trarizon.Toolkit.Deemo.Tester/Program.cs
+++ Trarizon.Toolkit.Deemo.Tester/Program.cs
@@ -1,9 +1,46 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Diagnostics;
+using System.Runtime.InteropServices;
 using Trarizon.Toolkit.Deemo.Algorithm;
 using Trarizon.Toolkit.Deemo.ChartModels;
 using Trarizon.Toolkit.Deemo.Tester;
 
-Benchmarks.Run();
+if (Chart.TryParseFromJson(File.ReadAllText(@"D:\Project Charts\Deemo's Note Memory\music\Deemo\No.5-2.N.M.S.T. collection #2\5.Fur War, Pur War.hard.txt"), out var chart)) {
+	Console.WriteLine("Loaded");
+
+	var notes = chart.Notes.Where(n => n.IsVisible).Select(n => n.IsSlide ? 0f : 200f).ToList();
+	var score = ScoreCalculation.Calc(CollectionsMarshal.AsSpan(notes));
+	Console.WriteLine(score);
+
+	var visibleNotes = chart.Notes.Where(n => n.IsVisible).ToArray();
+
+	var slides = visibleNotes.Aggregate((0, 0, 0, 0), ((int Index, int Count, int TailIndex, int MaxCount) aggr, Note n) =>
+	{
+		if (n.IsSlide) {
+			aggr.Count++;
+		}
+		else {
+			if (aggr.MaxCount < aggr.Count) {
+				aggr.MaxCount = int.Max(aggr.MaxCount, aggr.Count);
+				aggr.TailIndex = aggr.Index;
+			}
+			aggr.Count = 0;
+		}
+		aggr.Index++;
+		return aggr;
+	}, aggr => (aggr.Item3 - aggr.Item4)..aggr.Item3);
+
+	Console.WriteLine((visibleNotes.AsSpan(slides).ToArray().All(n => n.IsSlide)));
+
+	Console.WriteLine(visibleNotes.AsSpan(slides).Length);
+
+
+	var paint = ChartPainter.Paint(visibleNotes.AsSpan(slides).ToArray());
+	paint.Snapshot().Encode().SaveTo(File.OpenWrite(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "Img.png")));
+}
+
+
+// Benchmarks.Run();
 //if (Chart.TryParseFromJson(File.ReadAllText(@"D:\Project Charts\Deenote\Music\&Deemo\5.Elysian Volitation.hard.txt"), out var chart)) {
 //	Console.WriteLine("Loaded");
 //	File.WriteAllText(@"D:\Project Charts\Deenote\Music\&Deemo\5.Elysian Volitation.hard.json", chart.ToJson(Trarizon.Toolkit.Deemo.ChartVersion.DeemoV2));
